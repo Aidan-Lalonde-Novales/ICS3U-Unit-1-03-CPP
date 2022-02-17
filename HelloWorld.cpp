fefbb7d11018351b263b2bbd281865d08d3ad1b9
Aidan-Lalonde-Novales <aidan.lalonde.novales@mths.ca>
2022-02-17 22:39:20 +0000

    added cpp file

---
+++ HelloWorld.cpp
@@ -0,0 +1,12 @@
+// Copyright (c) 2022 Aidan Lalonde-Novales All rights reserved
+//
+// Created by Aidan Lalonde-Novales
+// Created on 2022-02-17
+// This is a "Hello, World!" program with proper style
+
+#include <iostream>
+
+int main() {
+  std::cout << "Hello, World!" << std::endl;
+  std::cout << "Goodbye, World!" << std::endl;
+}
